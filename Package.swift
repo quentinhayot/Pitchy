99c787f7db827f6ae574d23e2535fdfbf884e7ae
Quentin Hayot <quentinhayot@users.noreply.github.com>
2022-04-06 00:33:00 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,6 @@ let package = Package(
   name: "Pitchy",
   products: [
       .library(name: "Pitchy", targets: ["Pitchy"]),
-      .library(name: "Pitchy-Dynamic", type: .dynamic, targets: ["Pitchy"]),
   ],
   dependencies: [],
   targets: [
